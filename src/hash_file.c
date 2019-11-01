4da41f93c6faaf4c913ebff2461345f01f453c6d
FotiadisM <fotiadis.michalis20@gmail.com>
2019-11-01 17:37:31 +0200

    CreateIndex done

--- src/hash_file.c
+++ src/hash_file.c
@@ -9,9 +9,9 @@
 #define CALL_BF(call)       \
 {                           \
   BF_ErrorCode code = call; \
-  if (code != BF_OK) {         \
+  if (code != BF_OK) {      \
     BF_PrintError(code);    \
-    return HP_ERROR;        \
+    return HT_ERROR;        \
   }                         \
 }
 
@@ -22,6 +22,45 @@ HT_ErrorCode HT_Init() {
 
 HT_ErrorCode HT_CreateIndex(const char *filename, int buckets) {
   //insert code here
+  int fd;
+  char *data;
+  BF_Block *mBlock, *tmpBlock;
+  BF_Block_Init(&mBlock);
+  BF_Block_Init(&tmpBlock);
+
+  BF_Init(LRU);
+
+  CALL_BF(BF_CreateFile(filename));
+  CALL_BF(BF_OpenFile(filename, &fd));
+  CALL_BF(BF_AllocateBlock(fd, mBlock));
+  data = BF_Block_GetData(mBlock);
+  memcpy(data, "HT", 2);
+  memset(data + 2, 0, sizeof(int));
+  memset(data + 2 + 2*sizeof(int), buckets, sizeof(int));
+  BF_Block_SetDirty(mBlock);
+  CALL_BF(BF_UnpinBlock(mBlock));
+
+  CALL_BF(BF_AllocateBlock(fd, mBlock));
+  data = BF_Block_GetData(mBlock);
+  memset(data, 0, sizeof(int));
+  for(int i=0; i<buckets; i++) {
+    int k = i+2;
+    memcpy(data + sizeof(int) + i*sizeof(int), &k, sizeof(int));
+  }
+
+  BF_Block_SetDirty(mBlock);
+  CALL_BF(BF_UnpinBlock(mBlock));
+
+  // CALL_BF(BF_GetBlock(fd, 1, mBlock));
+  // data = BF_Block_GetData(mBlock);
+  // for(int i=0; i<buckets; i++) {
+  //   int num = *(int*)(data + sizeof(int) + i*sizeof(int));
+  //   printf("HEHE %d\n", num);
+  // }
+  // CALL_BF(BF_UnpinBlock(mBlock));
+
+  BF_Block_Destroy(&mBlock);
+  BF_Block_Destroy(&tmpBlock);
   return HT_OK;
 }
 
